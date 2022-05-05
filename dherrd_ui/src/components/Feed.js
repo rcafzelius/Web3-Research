adee61ea29f693ed6d8bfda739b35e1de1d1cce8
MatthewScott-jpg <scottydotty333@gmail.com>
2022-05-04 21:52:46 -0400

    Feed update

--- dherrd_ui/src/components/Feed.js
+++ dherrd_ui/src/components/Feed.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -13,43 +13,45 @@ import { useMoralis } from "react-moralis";
 import { BigNumber } from 'ethers';
 //add listeners to buttons
 
-function Feed(props){
+function Feed(props) {
+    const dherrdAddr = '0xB5088a0782Ea8c8E9c9a335589f64070D1DC1A15';
     const Web3Api = useMoralisWeb3Api();
-    const [postData, setPostData] = useState({posts: []});
+    const [postData, setPostData] = useState({ posts: [] });
     const [toggleAddPost, setToggleAddPost] = useState(false);
     const [newPost, setNewPost] = useState('');
-    const [somethingChanged] = useState(false);
+    const [toggleAddComment, setToggleAddComment] = useState({state:false,row:''});
+    const [newComment, setNewComment] = useState({row:'', comment:''});
+    const [somethingChanged, setSomethingChanged] = useState(false);
     const {
         Moralis
-      } = useMoralis();
+    } = useMoralis();
     //var oldaddr = "0xB22F31b14092fe1639EBb33c62C3320Fb71Bd9c3"
     //const addr = "0x63352EBE37b7cF82b2c2cEEA8903C049C7B4CD08";
-    useEffect(()=>{
-        async function fetchNFTs(){
+    useEffect(() => {
+        async function fetchNFTs() {
             const testnetNFTs = await Web3Api.Web3API.account.getNFTs({
                 chain: "rinkeby",
-              });
-              console.log(testnetNFTs)
-            }
+            });
+            console.log(testnetNFTs)
+        }
         fetchNFTs()
 
-        async function fetchPosts(){
-            const dherrdAddr = '0xB5088a0782Ea8c8E9c9a335589f64070D1DC1A15';
+        async function fetchPosts() {
             const totalPostOptions = {
                 contractAddress: dherrdAddr,
                 functionName: "totalPosts",
                 abi: dherrdABI,
                 params: {
                     _newMessage: "Hello Moralis",
-                  },
+                },
             }
             const totalPostCount = await Moralis.executeFunction(totalPostOptions).then(
-                (tpc)=>{
+                (tpc) => {
                     return tpc.toNumber()
                 }
             );
             const messages = []
-            for (let i = totalPostCount; i > 0; i --){
+            for (let i = totalPostCount; i > 0; i--) {
                 const options = {
                     contractAddress: dherrdAddr,
                     functionName: "getPost",
@@ -68,64 +70,111 @@ function Feed(props){
             })
         }
         fetchPosts()
+        //setSomethingChanged(false)
     }, [somethingChanged])
 
-    function handleNewPostChange(e){
+    function handleNewPostChange(e) {
         setNewPost(e.target.value)
     }
-    function handleAddPost(){
-        
+    async function handleAddPost() {
+        const createPostOptions = {
+            contractAddress: dherrdAddr,
+            functionName: "createPost",
+            abi: dherrdABI,
+            params: {
+                _content: newPost,
+            },
+        }
+        await Moralis.executeFunction(createPostOptions);
+        setToggleAddPost(false)
+        //setSomethingChanged(true)
     }
-    function handleUpvote(){
-        
+    function handleToggleAddComment(e,i) {
+        console.log(i)
+        setToggleAddComment({state: true, row: i});
     }
-    function handleDownvote(){
-        
+    function handleNewCommentChange(e,i) {
+        setNewComment({row: i, comment: e.target.value})
     }
-    function getNetLikes(upvotes, downvotes){
-            //console.log(upvotes.toNumber())
-            try{
-                const net = upvotes - downvotes
-                console.log(net)
-                return net
-    
-            } catch(TypeError){
-                return (2)
-            }
+    async function handleAddComment() {
+        //change this
+        const createCommentOptions = {
+            contractAddress: dherrdAddr,
+            functionName: "createPost",
+            abi: dherrdABI,
+            params: {
+                _content: newPost,
+            },
+        }
+        await Moralis.executeFunction(createCommentOptions);
+        setToggleAddComment(false)
+        //setSomethingChanged(true)
+    }
+    function handleUpvote() {
+
+    }
+    function handleDownvote() {
+
+    }
+    function getNetLikes(upvotes, downvotes) {
+        //console.log(upvotes.toNumber())
+        try {
+            const net = upvotes - downvotes
+            return net
+
+        } catch (TypeError) {
+            return (0)
+        }
     }
 
-    return(
+    return (
         <Container>
             <Row>
-                <Col sm={{offset:5}}>
+                <Col sm={{ offset: 5 }}>
                     <h3>Welcome to DeChat</h3>
                 </Col>
             </Row>
-            <ListGroup style={{width: '75%', margin:'auto'}}>
-                {postData.posts.map((item,i)=>{
-                    return(
+            <ListGroup style={{ width: '75%', margin: 'auto' }}>
+                {postData.posts.map((item, i) => {
+                    return (
                         <ListGroup.Item key={i} className="d-flex justify-content-between align-items-start">
-                                {item.content}
-                                <div style={{display: 'flex', flexDirection:'column'}}>
-                                    <Button size='sm' variant='outline-*' onClick={(e) => handleUpvote()}>
-                                        <FaArrowUp/>
+                            {item.content}
+                            <div>
+                                {toggleAddComment.state && toggleAddComment.row === item.postId?
+                                    <form onSubmit={handleAddComment}>
+                                        <input type="text" name="post" value={newComment.comment} onChange={(e) => handleNewCommentChange(e,item.postId)} />
+                                        <Button size='sm' variant='outline-light' style={{ color: 'black' }} onClick={handleAddComment}>
+                                            Add Comment
+                                        </Button>
+                                    </form> :
+                                    <Button size='sm' variant='outline-light' style={{ color: 'black' }} onClick={(e) => handleToggleAddComment(e,item.postId)}>
+                                        Add Comment
                                     </Button>
-                                    <Badge bg='light' text='dark'>{getNetLikes(item.likeCount, item.dislikeCount)}</Badge>
-                                    <Button size='sm' variant='outline-*' onClick={(e) => handleDownvote()}>
-                                        <FaArrowDown/>
-                                    </Button>
-                                </div>
+                                }
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <Button size='sm' variant='outline-*' onClick={(e) => handleUpvote()}>
+                                    <FaArrowUp />
+                                </Button>
+                                <Badge bg='light' text='dark'>{getNetLikes(item.likeCount, item.dislikeCount)}</Badge>
+                                <Button size='sm' variant='outline-*' onClick={(e) => handleDownvote()}>
+                                    <FaArrowDown />
+                                </Button>
+                            </div>
                         </ListGroup.Item>
-                    )})}
-                    {toggleAddPost?
+                    )
+                })}
+                {toggleAddPost ?
                     <form onSubmit={handleAddPost}>
-                        <input type="text" name="post" value={newPost} onChange={handleNewPostChange}/>
-                    </form>:
-                    <Button size='sm' variant='outline-light' style={{color:'black'}} onClick={setToggleAddPost}>
+                        <input type="text" name="post" value={newPost} onChange={handleNewPostChange} />
+                        <Button size='sm' variant='outline-light' style={{ color: 'black' }} onClick={handleAddPost}>
+                            Add Post
+                        </Button>
+                    </form> :
+                    <Button size='sm' variant='outline-light' style={{ color: 'black' }} onClick={setToggleAddPost}>
                         Add Post
                     </Button>
-                    
-                    }
+                }
             </ListGroup>
         </Container>
     )
